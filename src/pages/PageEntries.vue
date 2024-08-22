24fab4b15452981c23a5221a73a8d6227dd704f1
aleks-deets <oneofmycontacts5@gmail.com>
2024-08-22 23:45:12 +0300

    Created list of entries and display them on PageEntries.

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -1,8 +1,52 @@
 <template>
-  <q-page class="flex flex-center">
-    Entries
+  <q-page>
+    <div class="q-pa-md">
+      <q-list
+        bordered
+        separator
+      >
+        <q-item
+          v-for="entry in entries"
+          :key="entry.id"
+        >
+          <q-item-section>
+            {{ entry.name }}
+          </q-item-section>
+
+          <q-item-section side>
+            {{ entry.amount }}
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </q-page>
 </template>
 
 <script setup>
+
+  import { ref } from 'vue'
+
+  const entries = ref([
+    {
+      id: 'id0',
+      name: 'Salary',
+      amount: 4999.99
+    },
+    {
+      id: 'id1',
+      name: 'Rent',
+      amount: -999
+    },
+    {
+      id: 'id2',
+      name: 'Phone',
+      amount: -14.99
+    },
+    {
+      id: 'id3',
+      name: 'Unknown',
+      amount: 0
+    },
+  ])
+
 </script>
