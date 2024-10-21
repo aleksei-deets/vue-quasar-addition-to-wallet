ebdec85c6230a4d2eb43c5fb54bd53d4a91715bc
aleksei-deets <oneofmycontacts5@gmail.com>
2024-10-21 21:45:35 +0300

    Refactor: Split PageEntries into child components. Created component: Balance.

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -44,18 +44,7 @@
     <q-footer
       class="bg-transparent"
     >
-      <div class="row q-mb-sm q-px-md q-py-sm shadow-up-3">
-        <div class="col text-grey-7 text-h6">
-          Balance
-        </div>
-        <div
-          class="col text-h6 text-right"
-          :class="useAmountColorClass(storeEntries.balance)"
-        >
-          {{ useCurrencify(storeEntries.balance) }}
-        </div>
-      </div>
-
+      <Balance />
       <q-form 
         @submit="addEntryFormSubmit"
         class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary"
@@ -102,6 +91,7 @@
   import { useStoreEntries } from 'src/stores/storeEntries'
   import { useCurrencify } from 'src/use/useCurrencify'
   import { useAmountColorClass } from 'src/use/useAmountColorClass'
+  import Balance from 'src/components/Entries/Balance.vue'
 
   /*
     Stores
