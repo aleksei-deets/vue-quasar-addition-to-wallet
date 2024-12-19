aa33cb48f6790c8f7474d2d7ef5d919536a8efe1
aleksei-deets <oneofmycontacts5@gmail.com>
2024-12-19 15:28:56 +0300

    Feat: (WIP 2) Display paid balance.

--- src/components/Entries/Balance.vue
+++ src/components/Entries/Balance.vue
@@ -7,7 +7,23 @@
 			class="col text-h6 text-right"
 			:class="useAmountColorClass(storeEntries.balance)"
 		>
-			{{ useCurrencify(storeEntries.balance) }}
+			<div class="row">
+				<div class="col">
+					{{ useCurrencify(storeEntries.balance) }}
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col text-caption text-grey-6">
+					Paid:
+					<span 
+						class="text-weight-bold"
+						:class="useAmountColorClass(1234.99)"
+					>
+						{{ useCurrencify(1234.99) }}
+					</span>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
