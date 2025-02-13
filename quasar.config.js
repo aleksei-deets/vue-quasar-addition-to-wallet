467cb3633220fe8ee42f183d2d5c55ecce066ff6
aleksei-deets <oneofmycontacts5@gmail.com>
2025-02-13 18:26:06 +0300

    Install and initialize Firebase.

--- quasar.config.js
+++ quasar.config.js
@@ -11,6 +11,9 @@
 
 const { configure } = require('quasar/wrappers');
 
+require('dotenv').config();
+
+
 
 module.exports = configure(function (/* ctx */) {
   return {
@@ -74,6 +77,15 @@ module.exports = configure(function (/* ctx */) {
       // vitePlugins: [
       //   [ 'package-name', { ..pluginOptions.. }, { server: true, client: true } ]
       // ]
+
+      env: {
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+      },
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
