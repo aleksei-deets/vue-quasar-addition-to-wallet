7c4a11ce5d434a4a6521de70067df0b141679484
aleksei-deets <oneofmycontacts5@gmail.com>
2025-04-03 23:12:52 +0300

    deleteEntry method for Firestore database.
    The method now deletes the actual document with this entry id from the entries collection on Firestore.

--- src/stores/storeEntries.js
+++ src/stores/storeEntries.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref, reactive, computed, nextTick } from 'vue'
 import { Notify } from 'quasar'
-import { collection, onSnapshot, addDoc } from 'firebase/firestore'
+import { collection, onSnapshot, addDoc, doc, deleteDoc } from 'firebase/firestore'
 import { db } from 'src/firebase/firebase'
 
 const entriesCollectionRef = collection(db, 'entries')
@@ -102,10 +102,8 @@ export const useStoreEntries = defineStore('entries', () => {
     await addDoc(entriesCollectionRef, newEntry)
   }
 
-	const deleteEntry = entryId => {
-    const index = getEntryIndexById(entryId)
-    //console.log('index: ', index)
-    entries.value.splice(index, 1)
+  const deleteEntry = async entryId => {
+    await deleteDoc(doc(entriesCollectionRef, entryId))
     removeSlideItemIfExists(entryId)
     Notify.create({
       message: 'Entry deleted',
