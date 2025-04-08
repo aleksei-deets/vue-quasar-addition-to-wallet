3fc99fef71af8fbaa7a9f6e1720a4a1d4b2444e5
aleksei-deets <oneofmycontacts5@gmail.com>
2025-04-08 11:06:44 +0300

    fix: removed order numbers that were added for debugging.

--- src/components/Entries/Entry.vue
+++ src/components/Entries/Entry.vue
@@ -28,7 +28,7 @@
           { 'text-strike' : entry.paid }
         ]"
       >
-        {{ entry.name }} ({{ entry.order }})
+        {{ entry.name }}
         <!-- in <q-popup-edit> component use :model-value instead v-model. 
               To prevent the possibility of changing the data in the Pinia store. -->
         <!-- To Fix issue with offset style prop in QPopupEdit component
