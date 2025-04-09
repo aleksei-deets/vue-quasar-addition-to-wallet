17d1492a62c1375af33636de219e8622d28092e2
aleksei-deets <oneofmycontacts5@gmail.com>
2025-04-09 16:34:18 +0300

    Auth: Added tabs for login & register.

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -3,12 +3,27 @@
     <q-card 
       class="auth bg-primary text-white q-pa-lg"
     >
-      <ToolbarTitle />
+      <q-card-section>
+        <ToolbarTitle />
+      </q-card-section>
+      
+      <q-card-section>
+        <q-tabs
+          v-model="tab"
+          no-caps
+        >
+          <q-tab name="login" label="Login" />
+          <q-tab name="register" label="Register" />
+        </q-tabs>
+      </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import ToolbarTitle from 'src/components/Layout/ToolbarTitle.vue'
+
+const tab = ref('login')
 
 </script>
